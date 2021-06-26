a8fb77b5605bf380f45612253e396b2665c11560
danilsocol <danilsocol-2002@mail.ru>
2021-06-26 10:02:12 +0500

    1

---
+++ Проверка машин/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Проверка_машин
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listMachin = new List<string>();
+            listMachin.AddRange(File.ReadAllLines("Машины.txt"));
+
+            while (true)
+            {
+                string newMachin = Console.ReadLine();
+
+                for (int i = 0; i < listMachin.Count; i++)
+                {
+                    if (newMachin == listMachin[i])
+                    {
+                        Console.WriteLine("Такая машину уже есть");
+                        break;
+                    }
+                    else if (i == listMachin.Count - 1)
+                    {
+                        listMachin.Add(newMachin);
+                        File.WriteAllLines("Машины.txt", listMachin);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
